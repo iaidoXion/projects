098be158afb6218084f8f175c128b4329d14374f
iaidoXion <102495845+iaidoXion@users.noreply.github.com>
2022-04-08 11:57:13 +0900

    menu setting module 저장

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class MenuSetting(models.Model):
+    menuId = models.CharField(max_length=50)
+    menuName = models.CharField(max_length=200)
+    menuUrl = models.CharField(max_length=200)
+    menuUse = models.CharField(max_length=10)
+    menuNote = models.TextField()
+    menuCreateDate = models.DateTimeField()
