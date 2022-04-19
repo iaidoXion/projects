7429bdde587849670b7ff79ec05300d13764789e
AIDO I <aidoi@AIDOui-MacBookPro.local>
2022-04-19 00:33:35 -0700

    data change

--- web/migrations/0001_initial.py
+++ web/migrations/0001_initial.py
@@ -18,6 +18,7 @@ class Migration(migrations.Migration):
                 ('menuId', models.CharField(max_length=50)),
                 ('menuName', models.CharField(max_length=200)),
                 ('menuUrl', models.CharField(max_length=200)),
+                ('menuImg', models.CharField(max_length=200)),
                 ('menuUse', models.CharField(max_length=10)),
                 ('menuNote', models.TextField()),
                 ('menuCreateDate', models.DateTimeField()),
