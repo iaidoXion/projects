2292e37487a5b14343c7f4b2fef7554843d615c7
csk3830 <skchoi@xionits.com>
2022-05-30 10:38:18 +0900

    화살표 아이콘 색 변경, stack bar chart로 변경, 코드정리, dashboard layout 수정, mapchart 수정, 이상징후알림 layout 수정, network chart 및 data파일 추가, language 선택 버튼 분리, menu style 변경, nc 이미지 추가, 스타일 변경, user 아이콘 색 변경

--- web/static/js/chart/barchart.js
+++ web/static/js/chart/barchart.js
@@ -1,10 +1,157 @@
-function center(){
-    $("#barChart").css({"width" : "100% "});
-};
-
 function barChart(barChartData) {
 
-        var tooltip = d3
+
+var margin = {top: 10, right: 160, bottom: 25, left: 30};
+
+var width = 600 - margin.left - margin.right,
+    height = 175 - margin.top - margin.bottom;
+
+var svg = d3.select("#barChart")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+/* Data in strings like it would be if imported from a csv */
+
+var data = [
+  { year: "2006", redDelicious: "10", mcintosh: "15", oranges: "9" },
+  { year: "2007", redDelicious: "12", mcintosh: "18", oranges: "9" },
+  { year: "2016", redDelicious: "19", mcintosh: "17", oranges: "5" }
+];
+
+var parse = d3.time.format("%Y").parse;
+
+
+// Transpose the data into layers
+var dataset = d3.layout.stack()(["redDelicious", "mcintosh", "oranges"].map(function(fruit) {
+  return data.map(function(d) {
+    return {x: parse(d.year), y: +d[fruit]};
+  });
+}));
+
+
+// Set x, y and colors
+var x = d3.scale.ordinal()
+  .domain(dataset[0].map(function(d) { return d.x; }))
+  .rangeRoundBands([0, width-10], 0.02);
+
+var y = d3.scale.linear()
+  .domain([0, d3.max(dataset, function(d) {  return d3.max(d, function(d) { return d.y0 + d.y; });  })])
+  .range([height, 0]);
+
+var colors = ["#e18a0a", "#f6a631", "#f8c477"];
+
+
+// Define and draw axes
+var yAxis = d3.svg.axis()
+  .scale(y)
+  .orient("left")
+  .ticks(5)
+  .tickSize(0, 0)
+  .tickFormat( function(d) { return d } );
+
+var xAxis = d3.svg.axis()
+  .scale(x)
+  .orient("bottom")
+  .tickFormat(d3.time.format("%Y"));
+
+svg.append("g")
+  .attr("class", "y axis")
+  .call(yAxis);
+
+svg.append("g")
+  .attr("class", "x axis")
+  .attr("transform", "translate(0," + height + ")")
+  .call(xAxis);
+
+
+// Create groups for each series, rects for each segment
+var groups = svg.selectAll("g.cost")
+  .data(dataset)
+  .enter().append("g")
+  .attr("class", "cost")
+  .style("fill", function(d, i) { return colors[i]; });
+
+var rect = groups.selectAll("rect")
+  .data(function(d) { return d; })
+  .enter()
+  .append("rect")
+  .attr("x", function(d) { return x(d.x); })
+  .attr("y", function(d) { return y(d.y0 + d.y); })
+  .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); })
+  .attr("width", x.rangeBand())
+  .on("mouseover", function() { tooltip.style("display", null); })
+  .on("mouseout", function() { tooltip.style("display", "none"); })
+  .on("mousemove", function(d) {
+    var xPosition = d3.mouse(this)[0] - 15;
+    var yPosition = d3.mouse(this)[1] - 25;
+    tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+    tooltip.select("text").text(d.y);
+  });
+
+
+// Draw legend
+var legend = svg.selectAll(".legend")
+  .data(colors)
+  .enter().append("g")
+  .attr("class", "legend")
+  .attr("transform", function(d, i) { return "translate(-100," + i * 11 + ")"; });
+
+legend.append("rect")
+  .attr("x", width - 18)
+  .attr("width", 9)
+  .attr("height", 9)
+  .style("fill", function(d, i) {return colors.slice().reverse()[i];});
+
+legend.append("text")
+  .attr("x", width + -4)
+  .attr("y", 5)
+  .attr("dy", ".35em")
+  .style("text-anchor", "start")
+  .text(function(d, i) {
+    switch (i) {
+      case 0: return "Anjou pears";
+      case 1: return "Naval oranges";
+      case 2: return "McIntosh apples";
+      case 3: return "Red Delicious apples";
+    }
+  });
+
+
+// Prep the tooltip bits, initial display is hidden
+var tooltip = svg.append("g")
+  .attr("class", "tooltip")
+  .style("display", "none");
+
+tooltip.append("rect")
+  .attr("width", 30)
+  .attr("height", 20)
+  .attr("fill", "white")
+  .style("opacity", 0.5);
+
+tooltip.append("text")
+  .attr("x", 15)
+  .attr("dy", "1.2em")
+  .style("text-anchor", "middle")
+  .attr("font-size", "12px")
+  .attr("font-weight", "bold");
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*var tooltip = d3
                     .select('#barChart')
                     .append('div')
                     .attr('class', 'd3-tooltip')
@@ -89,7 +236,7 @@ function barChart(barChartData) {
             .append("text")
             .attr("x", svgWidth / 2 - widthPad)
             .attr("y", 50)
-            .text("")
+            .text("")*/
 
 };
 
