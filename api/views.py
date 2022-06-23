4e6c4e63ccdefff2ac9cfeaf4f3f54bce3c7ebf7
iaido <iaido@xionits.com>
2022-06-23 17:25:48 +0900

    디렉터리 구조 변경

--- api/views.py
+++ api/views.py
@@ -1,11 +1,11 @@
 from api.Call.Auth import SessionKey
 from api.Call.Extract import Asset as AssetAPI
 from api.Call.Sensor import Data as SensorAPI
-from module.Collection.Extract.Asset import Yesterday as EAY
-from module.Collection.Extract.Statistics import Yesterday as ESY, FiveDay as ESF
-from module.Collection.Transform.Asset import OrgDaily as AODT
-from module.Collection.Transform.Statistics import Yesterday as SYT, FiveDay as SFDT, Banner as SBT,  ChartData as SDT
-from module.Analysis.Statistics.Asset import DailyCount as ASDC, FiveDay as SFDS, BannerRoc as BR
+from DataParsing.Extract.Asset import Yesterday as EAY
+from DataParsing.Extract.Statistics import Yesterday as ESY, FiveDay as ESF
+from DataParsing.Transform.Asset import OrgDaily as AODT
+from DataParsing.Transform.Statistics import Yesterday as SYT, FiveDay as SFDT, Banner as SBT,  ChartData as SDT
+from Analysis.Statistics.Asset import DailyCount as ASDC, FiveDay as SFDS, BannerRoc as BR
 import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
