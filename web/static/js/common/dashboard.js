dda49a92274d0be4cd4196ce69af20f809e8c543
mendoda20 <mendoda20@gmail.com>
2022-07-22 10:51:21 +0900

    1. base.html  - datatable style 파일로 추가    (databases-1.10.12.css)
    2. navbar.html
     - navUserInfo 주석처리
     - 제품명 변경
    
     3. menu.css
     - navMenuWrap 너비 수정
     - navUserTime margin-left 수정
     - navUserTime margin-top 수정
    
     4. dashboard.js
     - datatable js 수정
    
     5. dashboard.css
     - popTable style 수정

--- web/static/js/common/dashboard.js
+++ web/static/js/common/dashboard.js
@@ -271,13 +271,29 @@ $("#demo2").als({
 
 //datatable
 $(document).ready(function(){
-    $(".popTable").DataTable({
-        "dom": 'ltfip',
-        "pageLength": 10,
-        "searching": false,
-        "ordering": false,
-        "info": true,
-        "paging": true,
-        "lengthChange": true,
+     $('.popTable').DataTable({
+        dom: 'litfp',
+        pageLength: 10,
+        searching: false,
+        ordering: false,
+        info: true,
+        paging: true,
+        lengthChange: false,
+        language: {
+                emptyTable: "데이터가 없어요.",
+                lengthMenu: "페이지당 _MENU_ 개씩 보기",
+                info: "현재 _START_ - _END_ / _TOTAL_건",
+                infoEmpty: "데이터 없음",
+                infoFiltered: "( _MAX_건의 데이터에서 필터링됨 )",
+                search: "에서 검색: ",
+                zeroRecords: "일치하는 데이터가 없어요.",
+                loadingRecords: "로딩중...",
+                processing:     "잠시만 기다려 주세요...",
+                paginate: {
+                    next: "다음",
+                    previous: "이전"
+                }
+        }
    });
+
 });
